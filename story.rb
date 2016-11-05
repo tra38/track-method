a18c8d8c2bbcb87b86d9e865e67e93abfc85c2ae
tra38 <tra38@nau.edu>
2016-11-04 20:32:47 -0500

    Update spelling and display commented-out function

--- story.rb
+++ story.rb
@@ -18,13 +18,13 @@ def add_line_breaks(array)
   end
 end
 
-fd_propaganda = extract_paragraphs(information["Frankenisten_Destroyer_Propaganda"])
+fd_propaganda = extract_paragraphs(information["Frakenstein_Destroyer_Propaganda"])
 pro_tech_propaganda = extract_paragraphs(information["Pro_Tech_Propaganda"])
 missions = extract_paragraphs(information["Missions"])
 backstories = extract_paragraphs(information["Worldbuilding"])
 
 # Set Up Track
-track = [{ state: "Frankenisten_Destroyer_Propaganda", array: fd_propaganda}, { state: "Pro_Tech_Propaganda", array: pro_tech_propaganda }, { state: "Missions", array: missions }, { state: "Worldbuilding", array: backstories }]
+track = [{ state: "Frakenstein_Destroyer_Propaganda", array: fd_propaganda }, { state: "Pro_Tech_Propaganda", array: pro_tech_propaganda }, { state: "Missions", array: missions }, { state: "Worldbuilding", array: backstories }]
 
 # set up blank story
 story = []
@@ -37,6 +37,7 @@ number_of_rounds.times do |round|
   number_of_checkpoints = track.length
   number_of_checkpoints.times do |checkpoint_index|
     checkpoint = track[checkpoint_index]
+    # story << introductory_paragraphs(checkpoint[:state])
     story << "We are now entering the #{checkpoint[:state]} state.\n\n"
     story << checkpoint[:array].pop
     story << "-----Filler----\n\n"
